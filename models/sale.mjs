0ecaf4cc80fbe4f7dec7ce5d872849e3e445f082
tyouse1 <tyouse1@live.com>
2024-02-05 11:23:34 -0500

    first commit...added the basics...set up Schema

---
+++ models/sale.mjs
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const saleSchema = new Schema(
+    {
+        saleDate: Date,
+        items: [],
+        storeLocation: String,
+        customer: {
+            gender: { type: String, enum: ['M', 'F'] },
+            age: Number,
+            email: String,
+            satisfaction: Number,
+        },
+        couponUsed: { Boolean, default: false },
+        purchaseMethod: String
+    }
+);
+const Grade = model("Sale", saleSchema);
+
+// make exportable to be accessed in index.mjs
+export default Sale;
